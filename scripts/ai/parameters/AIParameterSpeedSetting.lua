69ff8ce0be2455aebb00446099a58154ac116286
Peter Vaiko <github@vaiko.us>
2021-12-26 07:47:21 -0500

    Speed setting fix
    Do not change the max speed setting as it is global
    for all vehicles (at least as of now, later we may
    add vehicle specific maximums/minimums)
    
    #28 #58

--- scripts/ai/parameters/AIParameterSpeedSetting.lua
+++ scripts/ai/parameters/AIParameterSpeedSetting.lua
@@ -1,20 +1,10 @@
---- Speed setting
+--- Speed setting, currently no extra functionality over AIParameterSettingList, but may add in the future.
 ---@class AIParameterSpeedSetting : AIParameterSettingList
 AIParameterSpeedSetting = {}
 
 local AIParameterSpeedSetting_mt = Class(AIParameterSpeedSetting, AIParameterSettingList)
 
 function AIParameterSpeedSetting.new(data,vehicle,class,customMt)
-	data.max = AIParameterSpeedSetting.getMaxSpeed(vehicle,data.max)
 	local self = AIParameterSettingList.new(data,vehicle,class,customMt or AIParameterSpeedSetting_mt)
-
 	return self
-end
-
-function AIParameterSpeedSetting.getMaxSpeed(vehicle,default)
-	return vehicle and math.min(vehicle:getCruiseControlMaxSpeed(),default) or default
-end
-
-function AIParameterSpeedSetting:clone(...)
-	return AIParameterSpeedSetting.new(self.data,...)
 end
