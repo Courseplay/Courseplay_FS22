1bc5afa55336a929bbf15077504c9c832c43e3bd
schwiti6190 <58079399+schwiti6190@users.noreply.github.com>
2022-01-29 20:26:50 +0100

    Another merge fix

--- scripts/gui/CpAIFrameExtended.lua
+++ scripts/gui/CpAIFrameExtended.lua
@@ -349,3 +349,9 @@ function CpInGameMenuAIFrameExtended:executePickingCallback(...)
 end
 InGameMenuAIFrame.executePickingCallback = Utils.appendedFunction(InGameMenuAIFrame.executePickingCallback,
 															CpInGameMenuAIFrameExtended.executePickingCallback)
+
+function CpInGameMenuAIFrameExtended:onClickHotspot(element,hotspot)
+	if hotspot and hotspot:isa(CustomFieldHotspot) then 
+		hotspot:onClick()
+	end
+end
