10ac66ccae89eeb67148ccc24d6538ad52984602
schwiti6190 <58079399+schwiti6190@users.noreply.github.com>
2022-02-09 20:53:16 +0100

    Also apply the lane offset, if ad starts at the first wp.

--- scripts/specializations/CpAIFieldWorker.lua
+++ scripts/specializations/CpAIFieldWorker.lua
@@ -247,6 +247,9 @@ function CpAIFieldWorker:startCpAtFirstWp()
     self:updateAIFieldWorkerImplementData()
     if (self:hasCpCourse() and self:getCanStartCpFieldWork()) or self:getCanStartCpBaleFinder(spec.cpJobStartAtFirstWp:getCpJobParameters()) then
         spec.cpJobStartAtFirstWp:applyCurrentState(self, g_currentMission, g_currentMission.player.farmId, true)
+        
+        --- Applies the lane offset set in the hud, so ad can start with the correct lane offset.
+        spec.cpJobStartAtFirstWp:getCpJobParameters().laneOffset:setValue(spec.cpJob:getCpJobParameters().laneOffset:getValue())
         spec.cpJobStartAtFirstWp:setValues()
         g_client:getServerConnection():sendEvent(AIJobStartRequestEvent.new(spec.cpJobStartAtFirstWp, self:getOwnerFarmId()))
     end
