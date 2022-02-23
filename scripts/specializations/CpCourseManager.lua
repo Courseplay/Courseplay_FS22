ad998b3c220dfa30ed3d6a325ca45a83b658524b
Peter Vaiko <github@vaiko.us>
2022-02-23 10:34:14 -0500

    Course debug fixed

--- scripts/specializations/CpCourseManager.lua
+++ scripts/specializations/CpCourseManager.lua
@@ -272,7 +272,7 @@ function CpCourseManager:onDraw()
     --- Draw debug information of the generated fieldwork course.
     local course = self:getFieldWorkCourse()
     if course then 
-        if CpDebug:isChannelActive(CpDebug.DBG_COURSES, self.vehicle) then
+        if CpDebug:isChannelActive(CpDebug.DBG_COURSES, self) then
             local info = {
                 title = self:getCurrentCpCourseName(),
                 content = course:getDebugTable()
