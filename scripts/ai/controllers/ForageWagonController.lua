55edd0ba8e41276850a6d20c9ce99d43ec6bea1a
schwiti6190 <58079399+schwiti6190@users.noreply.github.com>
2022-03-10 19:12:12 +0100

    The forage wagon waits util it's completly filled now.

--- scripts/ai/controllers/ForageWagonController.lua
+++ scripts/ai/controllers/ForageWagonController.lua
@@ -2,39 +2,28 @@
 ---@class ForageWagonController : ImplementController
 ForageWagonController = CpObject(ImplementController)
 ForageWagonController.maxFillLevelPercentage = 0.95
+ForageWagonController.slowDownFillLevel = 200
+ForageWagonController.slowDownStartSpeed = 20
 function ForageWagonController:init(vehicle, forageWagon)
-	self.forageWagon = forageWagon
-    ImplementController.init(self, vehicle, self.forageWagon)
+    ImplementController.init(self, vehicle, forageWagon)
+    self.forageWagonSpec = forageWagon.spec_forageWagon
 end
 
 function ForageWagonController:update()
-    local fillLevel = 0
-    local capacity = 0
-    local dischargeNode = self.forageWagon:getCurrentDischargeNode()
-    if dischargeNode ~= nil then
-        fillLevel = self.forageWagon:getFillUnitFillLevel(dischargeNode.fillUnitIndex)
-        capacity = self.forageWagon:getFillUnitCapacity(dischargeNode.fillUnitIndex)
-    end
-
-    if fillLevel > capacity * self.maxFillLevelPercentage then
-        self:debug("Stopped Cp, as the unit is full.")
+    if self.implement:getFillUnitFreeCapacity(self.forageWagonSpec.fillUnitIndex) <= 0 then
+        self:debug("Stopped Cp, as the forage wagon is full.")
         self.vehicle:stopCurrentAIJob(AIMessageErrorIsFull.new())
     end
 end
 
 function ForageWagonController:getDriveData()
-    --- TODO: check if this is necessary.
-    if not self.forageWagon:getIsTurnedOn() then
-        if self.forageWagon.setFoldState then
-            -- unfold if there is something to unfold
-            self.forageWagon:setFoldState(-1, false)
-        end
-        if self.forageWagon:getCanBeTurnedOn() then
-            self:debug('Turning on machine')
-            self.forageWagon:setIsTurnedOn(true, false)
-        end
+    local fillLevel = self.implement:getFillUnitFillLevel(self.forageWagonSpec.fillUnitIndex)
+    local capacity = self.implement:getFillUnitCapacity(self.forageWagonSpec.fillUnitIndex)
+    local freeFillLevel = capacity - fillLevel
+    local maxSpeed
+    if freeFillLevel < self.slowDownFillLevel then
+        maxSpeed = 5 + freeFillLevel / self.slowDownFillLevel * self.slowDownStartSpeed
     end
-    return nil, nil, nil, nil
+    return nil, nil, nil, maxSpeed
 end
-
 
