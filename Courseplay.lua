528b3fd70030d14a1b5b93c5d0a32fd4ba7edbde
schwiti6190 <58079399+schwiti6190@users.noreply.github.com>
2022-06-29 09:54:34 +0200

    Removed another code hack for the help menu, that isn't needed anymore.

--- Courseplay.lua
+++ Courseplay.lua
@@ -455,12 +455,6 @@ function Courseplay:cpUnfreeze()
 	g_currentMission.controlledVehicle:unfreezeCp()
 end
 
---- Fixes global translations.
-function Courseplay.getText(i18n, superFunc, name, customEnv)
-	return superFunc(i18n, name, customEnv or Courseplay.MOD_NAME)
-end
-I18N.getText = Utils.overwrittenFunction(I18N.getText, Courseplay.getText)
-
 --- Registers all cp specializations.
 ---@param typeManager TypeManager
 function Courseplay.register(typeManager)
