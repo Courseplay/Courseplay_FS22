7f718dac5d44ecf66583e33537a37ce0de823dc9
schwiti6190 <58079399+schwiti6190@users.noreply.github.com>
2022-07-08 08:58:02 +0200

    Moved the console commands into a seperate lua file.

--- Courseplay.lua
+++ Courseplay.lua
@@ -7,7 +7,6 @@ Courseplay.baseXmlKey = "Courseplay"
 Courseplay.xmlKey = Courseplay.baseXmlKey.."."
 
 function Courseplay:init()
-	self:registerConsoleCommands()
 	g_gui:loadProfiles( Utils.getFilename("config/gui/GUIProfiles.xml", Courseplay.BASE_DIRECTORY) )
 
 	--- Base cp folder
@@ -105,6 +104,10 @@ function Courseplay:loadMap(filename)
 	--- Ugly hack to get access to the global AutoDrive table, as this global is dependent on the auto drive folder name.
 	self.autoDrive = FS22_AutoDrive and FS22_AutoDrive.AutoDrive
 	CpUtil.info("Auto drive found: %s", tostring(self.autoDrive~=nil))
+end
+
+function Courseplay:deleteMap()
+	g_devHelper:delete()
 end
 
 function Courseplay:setupGui()
@@ -289,170 +292,6 @@ function Courseplay.removePlayerActionEvents(player)
 		g_inputBinding:setShowMouseCursor(false)
 	end
 	player.wasCpMouseActive = nil
-end
-
-------------------------------------------------------------------------------------------------------------------------
--- Commands
-------------------------------------------------------------------------------------------------------------------------
-
-function Courseplay:registerConsoleCommands()
-	addConsoleCommand( 'cpAddMoney', 'adds money', 'addMoney', self)
-	addConsoleCommand( 'cpRestartSaveGame', 'Load and start a savegame', 'restartSaveGame', self)
-	addConsoleCommand( 'print', 'Print a variable', 'printVariable', self )
-	addConsoleCommand( 'printGlobalCpVariable', 'Print a global cp variable', 'printGlobalCpVariable', self )
-	addConsoleCommand( 'printVehicleVariable', 'Print g_currentMission.controlledVehicle.variable', 'printVehicleVariable', self )
-	addConsoleCommand( 'printImplementVariable', 'printImplementVariable <implement index> <variable>', 'printImplementVariable', self )
-	addConsoleCommand( 'printStrategyVariable', 'Print a CP drive strategy variable', 'printStrategyVariable', self )
-	addConsoleCommand( 'printAiPageVariable', 'Print a in game menu ai page variable.', 'printAiPageVariable', self )
-	addConsoleCommand( 'cpLoadFile', 'Load a lua file', 'loadFile', self )
-	addConsoleCommand( 'cpToggleDevHelper', 'Toggle development helper visual debug info', 'toggleDevHelper', self )
-	addConsoleCommand( 'cpSaveAllFields', 'Save all fields of the map to an XML file for offline debugging', 'cpSaveAllFields', self )
-	addConsoleCommand( 'cpReadVehicleConfigurations', 'Read custom vehicle configurations', 'cpReadVehicleConfigurations', self)
-	addConsoleCommand( 'cpSaveAllVehiclePositions', 'Save the position of all vehicles', 'cpSaveAllVehiclePositions', self)
-	addConsoleCommand( 'cpRestoreAllVehiclePositions', 'Restore the position of all vehicles', 'cpRestoreAllVehiclePositions', self)
-	addConsoleCommand( 'cpSetPathfinderDebug', 'Set pathfinder visual debug level (0-2)', 'cpSetPathfinderDebug', self )
-	addConsoleCommand( 'cpFreeze', 'Freeze the CP driver', 'cpFreeze', self )
-	addConsoleCommand( 'cpUnfreeze', 'Unfreeze the CP driver', 'cpUnfreeze', self )
-
-end
-
----@param saveGameNumber number
-function Courseplay:restartSaveGame(saveGameNumber)
-	if g_server then
-		doRestart(true, " -autoStartSavegameId " .. saveGameNumber)
-		CpUtil.info('Restarting savegame %d', saveGameNumber)
-	end
-end
-
----@param amount number
-function Courseplay:addMoney(amount)
-	g_currentMission:addMoney(amount ~= nil and tonumber(amount) or 0, g_currentMission.player.farmId, MoneyType.OTHER)	
-end
-
----Prints a variable to the console or a xmlFile.
----@param variableName string name of the variable, can be multiple levels
----@param maxDepth number maximum depth, 1 by default
----@param printToXML number should the variable be printed to an xml file ? (optional)
----@param printToSeparateXmlFiles number should the variable be printed to an xml file named after the variable ? (optional)
-function Courseplay:printVariable(variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-	if printToXML and tonumber(printToXML) and tonumber(printToXML)>0 then
-		CpUtil.printVariableToXML(variableName, maxDepth, printToSeparateXmlFiles)
-		return
-	end
-	CpUtil.printVariable(variableName, maxDepth)
-end
-
-function Courseplay:printVariableInternal(prefix, variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-	if not string.startsWith(variableName, ':') and not string.startsWith(variableName, '.') then
-		-- allow to omit the . at the beginning of the variable name.
-		prefix = prefix .. '.'
-	end
-	self:printVariable(prefix .. variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-end
-
--- make sure variableName is syntactically correct (can be appended to another variable)
-function Courseplay:ensureVariableNameSyntax(variableName)
-	if not variableName then
-		return ''
-	elseif not string.startsWith(variableName, ':') and not string.startsWith(variableName, '.') then
-		return '.' .. variableName
-	else
-		return variableName
-	end
-end
-
---- Print the variable in the selected vehicle's namespace
--- You can omit the dot for data members but if you want to call a function, you must start the variable name with a colon
-function Courseplay:printVehicleVariable(variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-	local prefix = variableName and 'g_currentMission.controlledVehicle' or 'g_currentMission'
-	variableName = variableName or 'controlledVehicle'
-	self:printVariableInternal( prefix, variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-end
-
---- Print an implement variable. If implement.object.variable exists, print that, otherwise implement.variable
----@param implementIndex number index in getAttachedImplements()
-function Courseplay:printImplementVariable(implementIndex, variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-	local prefix = string.format('g_currentMission.controlledVehicle:getAttachedImplements()[%d]', implementIndex)
-	local objectVariableName = string.format('%s.object%s', prefix, self:ensureVariableNameSyntax(variableName))
-	local var = CpUtil.getVariable(objectVariableName)
-	if var then
-		self:printVariable(objectVariableName, maxDepth, printToXML, printToSeparateXmlFiles)
-	else
-		local implementVariableName = string.format('%s%s', prefix, self:ensureVariableNameSyntax(variableName))
-		self:printVariable(implementVariableName, maxDepth, printToXML, printToSeparateXmlFiles)
-	end
-end
-
-function Courseplay:printStrategyVariable(variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-	local prefix = 'g_currentMission.controlledVehicle:getCpDriveStrategy()'
-	self:printVariableInternal( prefix, variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-end
-
-function Courseplay:printGlobalCpVariable(variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-	if variableName then 
-		self:printVariableInternal( 'g_Courseplay', variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-	else 
-		self:printVariable('g_Courseplay', maxDepth, printToXML, printToSeparateXmlFiles)
-	end
-end
-
-function Courseplay:printAiPageVariable(variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-	local prefix = 'g_currentMission.inGameMenu.pageAI'
-	self:printVariableInternal( prefix, variableName, maxDepth, printToXML, printToSeparateXmlFiles)
-end
-
---- Load a Lua file
---- This is to reload scripts without restarting the game.
-function Courseplay:loadFile(fileName)
-	fileName = fileName or 'reload.xml'
-	local path = Courseplay.BASE_DIRECTORY .. '/' .. fileName
-	if fileExists(path) then
-		g_xmlFile = loadXMLFile('loadFile', path)
-	end
-	if not g_xmlFile then
-		return 'Could not load ' .. path
-	else
-		local code = getXMLString(g_xmlFile, 'code')
-		local f = getfenv(0).loadstring('setfenv(1, '.. Courseplay.MOD_NAME .. '); ' .. code)
-		if f then
-			f()
-			return 'OK: ' .. path .. ' loaded.'
-		else
-			return 'ERROR: ' .. path .. ' could not be compiled.'
-		end
-	end
-end
-
-function Courseplay:toggleDevHelper()
-	g_devHelper:toggle()
-end
-
-function Courseplay:cpSaveAllFields()
-	CpFieldUtil.saveAllFields()
-end
-
-function Courseplay:cpReadVehicleConfigurations()
-	g_vehicleConfigurations:loadFromXml()
-end
-
-function Courseplay:cpSaveAllVehiclePositions()
-	g_devHelper:saveAllVehiclePositions()
-end
-
-function Courseplay:cpRestoreAllVehiclePositions()
-	g_devHelper:restoreAllVehiclePositions()
-end
-
-function Courseplay:cpSetPathfinderDebug(d)
-	PathfinderUtil.setVisualDebug(tonumber(d))
-end
-
-function Courseplay:cpFreeze()
-	g_currentMission.controlledVehicle:freezeCp()
-end
-
-function Courseplay:cpUnfreeze()
-	g_currentMission.controlledVehicle:unfreezeCp()
 end
 
 --- Registers all cp specializations.
