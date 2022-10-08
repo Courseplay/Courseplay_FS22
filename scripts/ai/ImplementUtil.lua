7f81d9df9cbc4ccb605fcb47ccc7db1bddce7530
Peter Vaiko <github@vaiko.us>
2022-10-08 14:57:41 -0400

    For the Z offset however, use the root vehicle

--- scripts/ai/ImplementUtil.lua
+++ scripts/ai/ImplementUtil.lua
@@ -408,10 +408,15 @@ function ImplementUtil.setPipeAttributes(object, implementWithPipe)
         end
         local dischargeIx = object.objectWithPipe:getPipeDischargeNodeIndex()
         local dischargeNode = object.objectWithPipe:getDischargeNodeByIndex(dischargeIx)
-        -- use combine so attached harvesters have the offset relative to the harvester's root node
+        -- for the side (X) offset, use combine so attached harvesters have the offset relative to the harvester's root node
         -- (and thus, does not depend on the angle between the tractor and the harvester)
-        object.pipeOffsetX, _, object.pipeOffsetZ = localToLocal(dischargeNode.node,
+        object.pipeOffsetX, _, _ = localToLocal(dischargeNode.node,
                 (implementWithPipe.getAIDirectionNode and implementWithPipe:getAIDirectionNode()) or implementWithPipe.rootNode, 0, 0, 0)
+        local referenceVehicle = implementWithPipe.rootVehicle
+        -- for the Z offset, we want the root vehicle, the offset for auger wagons and towed harvesters
+        -- should be relative to the tractor
+        _, _, object.pipeOffsetZ = localToLocal(dischargeNode.node,
+                (referenceVehicle.getAIDirectionNode and referenceVehicle:getAIDirectionNode()) or referenceVehicle.rootNode, 0, 0, 0)
         object.pipeOnLeftSide = object.pipeOffsetX >= 0
         CpUtil.debugVehicle(CpDebug.DBG_IMPLEMENTS, implementWithPipe, 'Pipe offset: x = %.1f, z = %.1f, on left side %s',
                 object.pipeOffsetX, object.pipeOffsetZ, tostring(object.pipeOnLeftSide))
@@ -425,7 +430,6 @@ function ImplementUtil.setPipeAttributes(object, implementWithPipe)
                 object.objectWithPipe:updatePipeNodes(999999, nil)
             end
         end
-        local referenceVehicle = implementWithPipe.rootVehicle
         if wasFolded and referenceVehicle.spec_foldable then
             ImplementUtil.foldAfterGettingWidth(referenceVehicle)
             -- fold and unfold quickly, if we don't do that, the implement start event won't unfold the combine pipe
