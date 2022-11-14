3f2358553b5d39d93d252e02940dd51a1985b963
Tensuko <theta-darkphoenix@gmx.net>
2022-11-14 10:56:13 +0100

    2nd measurment
    not sure how close the old remaining time was but like this it seems ok.

--- scripts/util/CpRemainingTime.lua
+++ scripts/util/CpRemainingTime.lua
@@ -6,12 +6,18 @@
 	- Every turn adds a flat turn penalty to the course length. Reduced by the course progress with a logarithm. (0 <-> ln(3)[~1.098..] )
 	- Global exponential factor applied to the course, depending on the progress left. (1 <-> e[~2.718..])
 
+	- Values measured with a simple course with one headland and 5m working width.
+	- small course with 136 wp is displayed with about 4min, but it takes about 6min.
+	- bigger course with 2766 wp is displayed with arround 1hour 29min, but takes about 1hour 15min.
+	- Conclusion: A rather small course have to too less displayed time, but thats just a few min off and not too bad.
+	- A bigger course will have more time displayed, but this is better then have a too less calculated time and have to wait for too long.
+
 ]]--
 ---@class CpRemainingTime
 CpRemainingTime = CpObject()
 CpRemainingTime.DISABLED_TEXT = ""
 CpRemainingTime.TURN_PENALTY = 20 -- Flat turn penalty
-CpRemainingTime.EXP_PENALTY_REDUCTION = 0.3 -- Reduces the impact of the exponential penalty.
+CpRemainingTime.EXP_PENALTY_REDUCTION = 0.2 -- Reduces the impact of the exponential penalty.
 
 function CpRemainingTime:init(vehicle)
 	self.vehicle = vehicle
@@ -40,6 +46,7 @@ end
 
 --- Estimate of the course time left with penalties increased.
 function CpRemainingTime:getRemainingCourseTime(course, ix) -- in seconds 
+
 	local optimalTime = self:getOptimalCourseTime(course, ix)
 
     return optimalTime * math.exp(1-course:getProgress(ix)) * self.EXP_PENALTY_REDUCTION
