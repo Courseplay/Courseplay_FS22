38da0293b4fc053c09c70264e88e8a5c10d1485c
Tim Nolle <tim.nolle@uni-ulm.de>
2022-11-20 22:30:41 +0100

    cleaned repo

--- reload.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set sourcefile=%1
-set outfile=reload.xml
-echo ^<code^> > %outfile%
-echo ^<![CDATA[ >> %outfile%
-type %sourcefile% >> %outfile%
-echo ]]^> >> %outfile%
-echo ^</code^> >> %outfile%
