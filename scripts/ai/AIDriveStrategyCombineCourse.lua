83dcb4fd57f488ec9e23a5277c2548c0e447acc3
Peter Vaiko <github@vaiko.us>
2023-03-28 15:14:06 -0400

    feat: combine unloader improvement
    I don't want to talk about this :)

--- scripts/ai/AIDriveStrategyCombineCourse.lua
+++ scripts/ai/AIDriveStrategyCombineCourse.lua
@@ -814,11 +814,7 @@ function AIDriveStrategyCombineCourse:callUnloaderWhenNeeded()
             local myEte = dToUnloadWaypoint / (self.vehicle:getSpeedLimit(true) / 3.6)
             self:debug('callUnloaderWhenNeeded: best unloader ETE at waypoint %d %.1fs, my ETE %.1fs',
                     tentativeRendezvousWaypointIx, bestEte, myEte)
-            if bestEte + 5 > myEte then
-                -- do not call too early (like minutes before we get there), only when it needs at least as
-                -- much time to get there as the combine (-5 seconds)
-                self:callUnloader(bestUnloader, tentativeRendezvousWaypointIx, bestEte)
-            elseif bestEte - 5 > myEte then
+            if bestEte - 5 > myEte then
                 -- I'll be at the rendezvous a lot earlier than the unloader which will almost certainly result in the
                 -- cancellation of the rendezvous.
                 -- So, set something up further away, with better chances,
@@ -837,6 +833,10 @@ function AIDriveStrategyCombineCourse:callUnloaderWhenNeeded()
                 else
                     self:debug('callUnloaderWhenNeeded: still can\'t find a good waypoint to meet the unloader')
                 end
+            elseif bestEte + 5 > myEte then
+                -- do not call too early (like minutes before we get there), only when it needs at least as
+                -- much time to get there as the combine (-5 seconds)
+                self:callUnloader(bestUnloader, tentativeRendezvousWaypointIx, bestEte)
             end
         end
     end
