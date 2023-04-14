65cb3bf48d9a04bcdc3ed86e85397acd41637fa6
schwiti6190 <58079399+schwiti6190@users.noreply.github.com>
2023-04-14 18:03:28 +0200

    Added another saftey nil check

--- scripts/ai/jobs/CpAIJobCombineUnloader.lua
+++ scripts/ai/jobs/CpAIJobCombineUnloader.lua
@@ -79,9 +79,9 @@ function CpAIJobCombineUnloader:applyCurrentState(vehicle, mission, farmId, isDi
 		self.cpJobParameters.fieldPosition:setPosition(x, z)
 	end
 	local x, z = self.cpJobParameters.fieldUnloadPosition:getPosition()
-
+	local angle = self.cpJobParameters.fieldUnloadPosition:getAngle()
 	-- no field position from the previous job, use the vehicle's current position
-	if x == nil or z == nil then
+	if x == nil or z == nil or angle == nil then
 		x, _, z = getWorldTranslation(vehicle.rootNode)
 		local dirX, _, dirZ = localDirectionToWorld(vehicle.rootNode, 0, 0, 1)
 		local angle = MathUtil.getYRotationFromDirection(dirX, dirZ)
