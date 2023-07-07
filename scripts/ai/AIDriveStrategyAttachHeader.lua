3b4275d9a24fd705d92b173fbf8d0520ea5a2898
schwiti6190 <58079399+schwiti6190@users.noreply.github.com>
2023-07-07 21:40:06 +0200

    Uses work with for reverse offset

--- scripts/ai/AIDriveStrategyAttachHeader.lua
+++ scripts/ai/AIDriveStrategyAttachHeader.lua
@@ -255,7 +255,7 @@ function AIDriveStrategyAttachHeader:attachHasFinished()
     if self.mode == self.MODES.ATTACH_HEADER_FROM_ATTACHED_TRAILER then 
         Markers.setMarkerNodes(self.vehicle)
         local length = AIUtil.getLength(self.vehicle)
-        local course = Course.createStraightReverseCourse(self.vehicle,math.max(1.5 * self.turningRadius, length),0)
+        local course = Course.createStraightReverseCourse(self.vehicle,math.max(self.workWidth/2 + 1.5 * self.turningRadius, length),0)
         self:startCourse(course, 1)
         self.state = self.states.REVERSING_FROM_CUTTER
     else 
