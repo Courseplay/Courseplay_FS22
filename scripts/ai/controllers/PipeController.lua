7035e306cc0f1e4b64aa22195e944852b4fc5095
schwiti6190 <58079399+schwiti6190@users.noreply.github.com>
2023-11-20 19:49:10 +0100

    Trailer height calculation bug
    The exact fill root node might be at the bottom fpr a few trailers, so we make sure to additionally check the height of the auto aim node with a max comparison.
    #2963

--- scripts/ai/controllers/PipeController.lua
+++ scripts/ai/controllers/PipeController.lua
@@ -165,7 +165,9 @@ function PipeController:getClosestExactFillRootNode()
 	local fillUnitIndex = self.pipeSpec.nearestObjectInTriggers.fillUnitIndex
     if objectId and fillUnitIndex then 
         local object = NetworkUtil.getObject(objectId)
-        return object and object:getFillUnitExactFillRootNode(fillUnitIndex)
+        if object then 
+            return object:getFillUnitExactFillRootNode(fillUnitIndex), object:getFillUnitAutoAimTargetNode(fillUnitIndex)
+        end
     end
 end
 
@@ -485,13 +487,10 @@ function PipeController:moveDependedPipePart(tool, dt)
     local toolNode = tool.node   
     local dischargeNode = self.dischargeNode.node
     local toolDischargeDist = calcDistanceFrom(toolNode, dischargeNode)
-    local exactFillRootNode = self:getClosestExactFillRootNode()
-
+    local exactFillRootNode, autoAimNode = self:getClosestExactFillRootNode()
     local tx, ty, tz = localToWorld(dischargeNode, 0, 0, 0)
     local _, gy, _ = localToWorld(toolNode, 0, 0, 0)
-   -- DebugUtil.drawDebugNode(dischargeNode, "dischargeNode")
     setTranslation(self.tempDependedNode, tx, gy, tz)
-   -- DebugUtil.drawDebugNode(self.tempDependedNode, "tempDependedNode")
 
     local toolTempDist = calcDistanceFrom(toolNode, self.tempDependedNode)
     --- Absolute angle difference needed to be adjustment.
@@ -515,9 +514,13 @@ function PipeController:moveDependedPipePart(tool, dt)
     end
 
     if exactFillRootNode then 
-     --   DebugUtil.drawDebugNode(exactFillRootNode, "exactFillRootNode")
         local _, gyT, _ = localToWorld(exactFillRootNode, 0, 0, 0)
-        gyT = gyT + 1
+        if autoAimNode then 
+            --- For some reason the exactFillRootNode might be at the bottom of the trailer ...
+            --- So we make sure to check an additional auto aim node 
+            local _, agyT, _ = localToWorld(autoAimNode, 0, 0, 0)
+            gyT = math.max(gyT, agyT) + 1
+        end
         if gyT > gy then
             local d = gyT - gy
             local beta = math.asin(d/toolDischargeDist)
@@ -528,9 +531,6 @@ function PipeController:moveDependedPipePart(tool, dt)
             end
         end
     end
-  --  if g_currentMission.controlledVehicle and g_currentMission.controlledVehicle == self.vehicle then
-  --      self:debug("Move depended: rotTarget: %.2f, oldRot: %.2f, rotMin: %.2f, rotMax: %.2f", targetRot, oldRot, tool.rotMin, tool.rotMax)
-  --  end
     ImplementUtil.moveMovingToolToRotation(self.implement, tool, dt, MathUtil.clamp(targetRot, tool.rotMin, tool.rotMax))
 end
 
@@ -545,15 +545,11 @@ function PipeController:movePipeUp(tool, childToolNode, dt)
     local toolNode = tool.node   
     local toolChildToolDist = calcDistanceFrom(toolNode, childToolNode)
 
-  --  DebugUtil.drawDebugNode(childToolNode, "childToolNode")
-  --  DebugUtil.drawDebugNode(toolNode, "toolNode")
-
-    local exactFillRootNode = self:getClosestExactFillRootNode()
+    local exactFillRootNode, autoAimNode = self:getClosestExactFillRootNode()
    
     local tx, ty, tz = localToWorld(childToolNode, 0, 0, 0)
     local gx, gy, gz = localToWorld(toolNode, 0, 0, 0)
     setTranslation(self.tempBaseNode, gx, ty, gz)
-  --  DebugUtil.drawDebugNode(self.tempBaseNode, "tempNode")
 
     local toolTempDist = calcDistanceFrom(toolNode, self.tempBaseNode)
     --- Absolute angle difference needed to be adjustment.
@@ -576,24 +572,24 @@ function PipeController:movePipeUp(tool, childToolNode, dt)
     end
 
     if exactFillRootNode then 
-    --    DebugUtil.drawDebugNode(exactFillRootNode, "exactFillRootNode")
+        DebugUtil.drawDebugNode(exactFillRootNode, "exactFillRootNode")
         local gxT, gyT, gzT = localToWorld(exactFillRootNode, 0, 0, 0)
-        gyT = gyT + 2
+        if autoAimNode then 
+            --- For some reason the exactFillRootNode might be at the bottom of the trailer ...
+            --- So we make sure to check an additional auto aim node 
+            local _, agyT, _ = localToWorld(autoAimNode, 0, 0, 0)
+            gyT = math.max(gyT, agyT) + 2
+        end
         local terrainHeight = getTerrainHeightAtWorldPos(g_currentMission.terrainRootNode, gxT, 0, gzT) + 4
         gyT = math.max(gyT, terrainHeight)
         local offset = gyT - gy
         if gyT < ty then
             local d = math.abs(gyT - gy + offset)
             local beta = math.asin(d/toolChildToolDist)
-       --     DebugUtil.drawDebugLine(gxT, gyT, gzT, gx, gy, gz, 0, 0, 1)
-       --     DebugUtil.drawDebugLine(tx, ty, tz, gxT, gyT, gzT, 0, 0, 1)
             targetRot = oldRot - beta
             if not self.pipeOnLeftSide then
                 targetRot = oldRot + beta
             end
-        --    if g_currentMission.controlledVehicle and g_currentMission.controlledVehicle == self.vehicle then
-        --        self:debug("Move up: rotTarget: %.2f, oldRot: %.2f, rotMin: %.2f, rotMax: %.2f", targetRot, oldRot, tool.rotMin, tool.rotMax)
-        --    end
         end
     end
     ImplementUtil.moveMovingToolToRotation(self.implement, tool, dt, MathUtil.clamp(targetRot, tool.rotMin, tool.rotMax))
