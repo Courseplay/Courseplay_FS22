e5a43a691f545981be14a7b69a2a8c3cca0bc8e7
Peter Vaiko <github@vaiko.us>
2024-06-29 07:26:06 -0400

    chore: lowercase folders

--- scripts/courseGenerator/test/include.lua
+++ scripts/courseGenerator/test/include.lua
@@ -1,3 +1,3 @@
-package.path = package.path .. ";../Geometry/?.lua;../../geometry/?.lua;../Genetic/?.lua;..?.lua;../?.lua;../../?.lua;../../pathfinder/?.lua;../../util/?.lua"
+package.path = package.path .. ";../geometry/?.lua;../../geometry/?.lua;../genetic/?.lua;..?.lua;../?.lua;../../?.lua;../../pathfinder/?.lua;../../util/?.lua"
 lu = require("luaunit")
 dofile('require.lua')
