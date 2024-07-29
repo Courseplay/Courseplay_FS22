13b6d7f0e437ddef81baa848cf377b461f444060
Tensuko <theta-darkphoenix@gmx.net>
2024-07-29 11:58:11 +0200

    Update FieldworkContext.lua

--- scripts/courseGenerator/FieldworkContext.lua
+++ scripts/courseGenerator/FieldworkContext.lua
@@ -81,7 +81,7 @@ function FieldworkContext:setHeadlandsWithRoundCorners(nHeadlandsWithRoundCorner
 end
 
 ---@param nIslandHeadlands number of headlands to generate around field islands
-function FieldworkContext:setnIslandHeadlands(nIslandHeadlands)
+function FieldworkContext:setIslandHeadlands(nIslandHeadlands)
     self.nIslandHeadlands = nIslandHeadlands
     return self
 end
